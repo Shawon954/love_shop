44a60f7ca2ba780d9a3df03ad195417f2c734ea7
shawon <shawoncse954@gmail.com>
2023-11-25 12:54:10 +0600

    Login screen Completed

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -2,19 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+  final loginkey = GlobalKey<FormState>();
 
-  final GlobalKey loginkey = GlobalKey<FormState>();
-
-  late TextEditingController loginemailcontroller,loginpasswordcontroller;
+  late TextEditingController loginemailcontroller, loginpasswordcontroller;
 
   var loginemail = "";
   var Loginpassword = "";
 
+  var ispassword = false.obs;
 
   @override
   void onInit() {
-     loginemailcontroller = TextEditingController();
-     loginpasswordcontroller = TextEditingController();
+    loginemailcontroller = TextEditingController();
+    loginpasswordcontroller = TextEditingController();
+
+    show();
+
     super.onInit();
   }
 
@@ -30,18 +33,29 @@ class LoginController extends GetxController {
     loginemailcontroller.clear();
   }
 
-  String? validatoremail(value){
-    if(!GetUtils.isEmail(value)){
+  String? validatoremail(value) {
+    if (!GetUtils.isEmail(value)) {
       return "Provide valid email";
     }
     return null;
   }
 
-  String? validatorpassword(value){
-    if(value.length<=6){
+  String? validatorpassword(value) {
+    if (value.length <= 6) {
       return "Password must be 6 characters";
     }
     return null;
   }
 
+  void checklogin() {
+    final isValid = loginkey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    loginkey.currentState!.save();
+  }
+
+  void show() {
+    ispassword.value = !ispassword.value;
+  }
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:get/get.dart';
 import 'package:love_shop/app/app_image/appimage.dart';
 import 'package:love_shop/app/app_string/app_string.dart';
 import 'package:love_shop/app/utils/base_size/basesize.dart';
 import 'package:love_shop/app/utils/text_style/style.dart';
-
 import '../../../app_color/app_colors.dart';
+import '../../../routes/app_pages.dart';
 import '../../../widget/backround/screen_backround.dart';
 import '../../../widget/button/button.dart';
 import '../controllers/login_controller.dart';
@@ -16,9 +15,12 @@ class LoginView extends GetView<LoginController> {
   final LoginController loginController = Get.put(LoginController());
 
   LoginView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
+    final Size size = MediaQuery
+        .of(context)
+        .size;
     return SafeArea(
       child: BackoundScaffold(
         extendBody: true,
@@ -44,7 +46,7 @@ class LoginView extends GetView<LoginController> {
                   style: textStyle(
                       fontsize: 18.sp,
                       fontWidget: FontWeight.w600,
-                      color: AppColor.textDark70,
+                      color: AppColor.textDark60,
                       letterSpacing: 1.2),
                 ),
                 SizedBox(
@@ -60,7 +62,7 @@ class LoginView extends GetView<LoginController> {
                       children: [
                         Padding(
                           padding:
-                              const EdgeInsets.only(left: BaseSize.Space_8),
+                          const EdgeInsets.only(left: BaseSize.Space_8),
                           child: Text(
                             AppString.emailtext,
                             style: textStyle(
@@ -75,21 +77,34 @@ class LoginView extends GetView<LoginController> {
                         TextFormField(
                           controller: controller.loginemailcontroller,
                           keyboardType: TextInputType.emailAddress,
+
+                          onSaved: (val){
+
+                            controller.Loginpassword =val!;
+                          },
+                          validator: (val){
+                          return controller.validatoremail (val!);
+                          },
                           decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            hintText: "Enter your email",
+                            hintStyle: textStyle(fontsize: 12.sp,fontWidget: FontWeight.w400,color:AppColor.textDark40),
                             prefixIcon: Icon(
                               Icons.email,
                               color: AppColor.primaryLighter,
                             ),
                             focusedBorder: OutlineInputBorder(
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(12)),
+                                BorderRadius.all(Radius.circular(12)),
                                 borderSide: BorderSide(
                                   color: AppColor.shadowDark40,
                                   width: 1.8,
                                 )),
                             enabledBorder: OutlineInputBorder(
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
+                              BorderRadius.all(Radius.circular(15)),
                               borderSide: BorderSide(
                                   color: AppColor.shadowDark50, width: 1.8),
                             ),
@@ -100,7 +115,7 @@ class LoginView extends GetView<LoginController> {
                         ),
                         Padding(
                           padding:
-                              const EdgeInsets.only(left: BaseSize.Space_8),
+                          const EdgeInsets.only(left: BaseSize.Space_8),
                           child: Text(
                             AppString.passwordtext,
                             style: textStyle(
@@ -112,144 +127,183 @@ class LoginView extends GetView<LoginController> {
                         SizedBox(
                           height: BaseSize.Space_5,
                         ),
-                        TextFormField(
-                          controller: controller.loginpasswordcontroller,
-                          keyboardType: TextInputType.emailAddress,
-                          cursorColor: AppColor.textDark80,
-                          autocorrect: true,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.lock,
-                              color: AppColor.primaryLighter,
-                            ),
-                            focusedBorder: OutlineInputBorder(
+                        Obx(() {
+                          return TextFormField(
+                            obscureText: controller.ispassword.value,
+                            controller: controller.loginpasswordcontroller,
+                            keyboardType: TextInputType.text,
+                            cursorColor: AppColor.textDark80,
+                            autocorrect: true,
+                           onSaved: (value){
+                              controller.Loginpassword= value!;
+                           },
+                           validator: (val){
+                             return controller.validatorpassword(val!);
+                           },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+
+                              hintText: "Enter your password",
+                              hintStyle: textStyle(fontsize: 12.sp,fontWidget: FontWeight.w400,color:AppColor.textDark40),
+                              suffixIcon: Obx(() {
+                                return IconButton(
+                                    onPressed: () {
+                                      controller.show();
+                                    },
+                                    icon: controller.ispassword.value ? Icon(
+                                        Icons.visibility_off) : Icon(
+                                        Icons.visibility));
+                              }),
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: AppColor.primaryLighter,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(12)),
+                                  borderSide: BorderSide(
+                                    color: AppColor.shadowDark40,
+                                    width: 1.8,
+                                  )),
+                              enabledBorder: OutlineInputBorder(
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(12)),
+                                BorderRadius.all(Radius.circular(15)),
                                 borderSide: BorderSide(
-                                  color: AppColor.shadowDark40,
-                                  width: 1.8,
-                                )),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              borderSide: BorderSide(
-                                  color: AppColor.shadowDark50, width: 1.8),
+                                    color: AppColor.shadowDark50, width: 1.8),
+                              ),
                             ),
+                          );
+                        }),
+
+                        SizedBox(
+                          height: BaseSize.Space_5,
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                AppString.forgotpassword,
+                                style: textStyle(
+                                  fontWidget: FontWeight.w400,
+                                  fontsize: 14.sp,
+                                ),
+                              )),
+                        ),
+                        SizedBox(
+                          height: BaseSize.Space_8,
+                        ),
+                        Center(
+                          child: Button(
+                            height: size.height / 15.0,
+                            width: size.width / 1.8,
+                            child: Text(
+                              AppString.logintext,
+                              style: textStyle(
+                                  fontWidget: FontWeight.w500,
+                                  fontsize: 18.sp,
+                                  letterSpacing: 1.1),
+                            ),
+                            onPressed: () {
+                              controller.checklogin();
+                            },
                           ),
                         ),
+                        SizedBox(
+                          height: BaseSize.Space_15,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Divider(
+                                  thickness: 1.5,
+                                  color: AppColor.textDark40,
+                                  indent: 50,
+                                )),
+                            Text(
+                              AppString.or,
+                              style: textStyle(
+                                fontsize: 18.sp,
+                                fontWidget: FontWeight.w500,
+                                color: AppColor.textDark70,
+                              ),
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: Divider(
+                                  thickness: 1.5,
+                                  color: AppColor.textDark40,
+                                  endIndent: 50,
+                                )),
+                          ],
+                        ),
+                        SizedBox(
+                          height: BaseSize.Space_15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: CircleAvatar(
+                                  backgroundColor: Colors.grey.shade300,
+                                  radius: 28.r,
+                                  child: Image.asset(
+                                    AppImage.facebook_icon,
+                                    height: 40.h,
+                                    width: 40.w,
+                                  )),
+                            ),
+                            SizedBox(width: BaseSize.Space_20,),
+                            GestureDetector(
+                              onTap: () {},
+                              child: CircleAvatar(
+                                  backgroundColor: Colors.grey.shade300,
+                                  radius: 28.r,
+                                  child: Image.asset(
+                                    AppImage.gmail_icon,
+                                    height: 40.h,
+                                    width: 40.w,
+                                  )),
+                            ),
+
+                          ],
+                        ),
+                        SizedBox(
+                          height: BaseSize.Space_15,
+                        ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () => Get.toNamed(Routes.signup),
+                            child: RichText(
+                                text: TextSpan(
+                                    text: AppString.donthave,
+                                    style: textStyle(
+                                        fontsize: 12.sp,
+                                        color: AppColor.textDark40,
+                                        fontWidget: FontWeight.w500),
+                                    children: [
+                                      TextSpan(
+                                          text: AppString.signup,
+                                          style: textStyle(
+                                              fontsize: 14.sp,
+                                              color: AppColor.textDark70,
+                                              fontWidget: FontWeight.w500)),
+                                    ])),
+                          ),
+                        ),
+                        SizedBox(
+                          height: BaseSize.Space_15,
+                        ),
+
                       ],
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: BaseSize.Space_5,
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        AppString.forgotpassword,
-                        style: textStyle(
-                          fontWidget: FontWeight.w400,
-                          fontsize: 14.sp,
-                        ),
-                      )),
-                ),
-                SizedBox(
-                  height: BaseSize.Space_8,
-                ),
-                Button(
-                  height: size.height / 15.0,
-                  width: size.width / 1.8,
-                  child: Text(
-                    AppString.logintext,
-                    style: textStyle(
-                        fontWidget: FontWeight.w500,
-                        fontsize: 18.sp,
-                        letterSpacing: 1.1),
-                  ),
-                  onPressed: () {},
-                ),
-                SizedBox(
-                  height: BaseSize.Space_15,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: Divider(
-                          thickness: 1.5,
-                          color: AppColor.textDark40,
-                          indent: 50,
-                        )),
-                    Text(
-                      AppString.or,
-                      style: textStyle(
-                        fontsize: 18.sp,
-                        fontWidget: FontWeight.w500,
-                        color: AppColor.textDark70,
-                      ),
-                    ),
-                    Expanded(
-                        flex: 1,
-                        child: Divider(
-                          thickness: 1.5,
-                          color: AppColor.textDark40,
-                          endIndent: 50,
-                        )),
-                  ],
-                ),
-                SizedBox(
-                  height: BaseSize.Space_15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: CircleAvatar(
-                          backgroundColor: Colors.grey.shade300,
-                          radius: 28.r,
-                          child: Image.asset(
-                            AppImage.facebook_icon,
-                            height: 40.h,
-                            width: 40.w,
-                          )),
-                    ),
-                    SizedBox(width: BaseSize.Space_20,),
-                    InkWell(
-                      onTap: () {},
-                      child: CircleAvatar(
-                          backgroundColor: Colors.grey.shade300,
-                          radius: 28.r,
-                          child: Image.asset(
-                            AppImage.gmail_icon,
-                            height: 40.h,
-                            width: 40.w,
-                          )),
-                    ),
 
-                  ],
-                ),
-                SizedBox(
-                  height: BaseSize.Space_15,
-                ),
-                RichText(
-                    text: TextSpan(
-                        text: AppString.donthave,
-                        style: textStyle(
-                            fontsize: 12.sp,
-                            color: AppColor.textDark40,
-                            fontWidget: FontWeight.w500),
-                        children: [
-                      TextSpan(
-                          text: AppString.signup,
-                          style: textStyle(
-                              fontsize: 14.sp,
-                              color: AppColor.textDark70,
-                              fontWidget: FontWeight.w500)),
-                    ])),
               ],
             ),
           ),
