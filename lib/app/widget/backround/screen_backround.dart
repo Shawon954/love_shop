0d4f58f55589dfee4bb4d0324791bc9459c5d10e
shawon <shawoncse954@gmail.com>
2023-11-27 17:16:43 +0600

    App homepage

--- lib/app/widget/backround/screen_backround.dart
+++ lib/app/widget/backround/screen_backround.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:love_shop/app/app_color/app_colors.dart';
+import 'package:love_shop/app/app_image/appimage.dart';
 
 class BackoundScaffold extends StatelessWidget {
 
@@ -25,11 +27,13 @@ class BackoundScaffold extends StatelessWidget {
             ), //BoxDecoration
             child: UserAccountsDrawerHeader(
               decoration: BoxDecoration(color: Colors.green),
+              
               accountName: Text(
                 "Abhishek Mishra",
                 style: TextStyle(fontSize: 18),
               ),
               accountEmail: Text("abhishekm977@gmail.com"),
+
               currentAccountPictureSize: Size.square(50),
               currentAccountPicture: CircleAvatar(
                 backgroundColor: Color.fromARGB(255, 165, 255, 137),
@@ -41,47 +45,37 @@ class BackoundScaffold extends StatelessWidget {
             ), //UserAccountDrawerHeader
           ), //DrawerHeader
           ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text(' My Profile '),
+            hoverColor: AppColor.primary,
+
+            selectedTileColor: Colors.amber,
+            leading: const ImageIcon(AssetImage(AppImage.homeoutlineicon,),color: AppColor.buttoncolor,),
+            title: const Text(' Home '),
             onTap: () {
-              Navigator.pop(context);
+
             },
           ),
           ListTile(
-            leading: const Icon(Icons.book),
-            title: const Text(' My Course '),
+            leading: const ImageIcon(AssetImage(AppImage.passionoutlineicon,),color: AppColor.buttoncolor,),
+            title: const Text(' Wishlist '),
             onTap: () {
-              Navigator.pop(context);
+
             },
           ),
           ListTile(
             leading: const Icon(Icons.workspace_premium),
-            title: const Text(' Go Premium '),
+            title: const Text(' Order '),
             onTap: () {
-              Navigator.pop(context);
+
             },
           ),
           ListTile(
-            leading: const Icon(Icons.video_label),
-            title: const Text(' Saved Videos '),
+            leading: const ImageIcon(AssetImage(AppImage.trolleyoutlineicon,),color: AppColor.buttoncolor,),
+            title: const Text(' Cart '),
             onTap: () {
-              Navigator.pop(context);
+
             },
           ),
-          ListTile(
-            leading: const Icon(Icons.edit),
-            title: const Text(' Edit Profile '),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('LogOut'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
+
         ],
       ),
     ), //Dra,
